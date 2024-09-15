4d1562a17f00118030a79e7bbb6b86881d02e0bd
minhnguyennn <minh842657913@gmail.com>
2024-09-16 00:02:11 +0700

    Finish Title Component

--- edusity/src/App.jsx
+++ edusity/src/App.jsx
@@ -1,12 +1,14 @@
 import Hero from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/Navbar";
 import Programs from "./Components/Programs/Programs";
+import Title from "./Components/Tite/Title";
 export default function App() {
   return (
     <div>
       <Navbar />
       <Hero />
       <div className="container">
+        <Title subTitle="Our PROGRAM" title = "What We Offer" />
         <Programs />
       </div>
     </div>
