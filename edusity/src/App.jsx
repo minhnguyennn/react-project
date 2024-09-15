f53ce5dc366276c6887beee5bf65ec0cee5871de
minhnguyennn <minh842657913@gmail.com>
2024-09-15 23:51:17 +0700

    Finish Programs Component

--- edusity/src/App.jsx
+++ edusity/src/App.jsx
@@ -1,8 +1,14 @@
 import Hero from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/Navbar";
+import Programs from "./Components/Programs/Programs";
 export default function App() {
-  return<div>
-    <Navbar/>
-    <Hero/>
-  </div>
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <div className="container">
+        <Programs />
+      </div>
+    </div>
+  );
 }
